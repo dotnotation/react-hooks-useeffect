8c8ca327de062521591dd35f457d1282564c22cd
Laura Berge <lauraeberge@gmail.com>
2021-06-09 17:55:10 -0500

    pre-useeffect lecture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Home from "./components/Home";
+import Counter from "./components/Counter"
 import './App.css';
 
 function App() {
+  const [page, setPage] = useState("home")
+
+  const pages = {
+    home: "counter",
+    counter: "home"
+  }
+
+  function renderPage() {
+    switch (page) {
+      case "home":
+        return <Home />;
+      default:
+        return <Counter />;;
+    }
+  }
+
+  function togglePage(){
+    setPage(prevPage => pages[prevPage])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <button onClick={togglePage}>{pages[page]}</button>
+      </nav>
+      {renderPage()}
     </div>
   );
 }
